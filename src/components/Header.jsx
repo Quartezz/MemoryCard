cb2cd15bbcd1f321b4c877c2dcc807e4287a3371
Quartezz <kamil.machel03@gmail.com>
2023-10-03 00:26:06 +0200

    changed footer link to be in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
-import "../styles/Header.scss"
+import "../styles/Header.scss";
 
 export default function Header() {
-    return(
-        <header>
-            <img src="logo.png" alt="lol" />
-            <h1 className="title">Memory Card Game</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src="logo.png" alt="lol" />
+      <a
+        href="https://github.com/Quartezz"
+        target="_blank"
+        className="github-link"
+      >
+        <h2 className="nick">Quartezz</h2>
+        <i className="fab fa-github"></i>
+      </a>
+      <h1 className="title">Memory Card Game</h1>
+    </header>
+  );
 }
