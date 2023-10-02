f4c8d41af596af2be580874945eb6d466e6004a8
Quartezz <kamil.machel03@gmail.com>
2023-10-02 20:53:06 +0200

    added functions

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,12 @@
 import Cards from "./Cards"
-import { useState } from "react"
+import { useState, useCallback } from "react"
 import cardsData from "../data/cards"
 
 export default function Game() {
+    const [cards, setCards] = useState(generateNewCards(cardsData))
+    const cardsInUse = cards.filter((card) => card.isCardInUse)
+    const [currentScore, setCurrentScore] = useState(0)
+
     function getShuffledCards(targetCards) {
         const copy = [...targetCards]
         for (let i = copy.length - 1; i > 0; i -= 1) {
@@ -32,27 +36,41 @@ export default function Game() {
         return newCards
     }
 
-    function setCardsClicked(targetCards, cardToUpdate) {
-        const updatedCards = targetCards.map((card) => {
-            if (card.id === cardToUpdate.id) {
-                return { ...cardToUpdate, hasClicked: true }
-            }
+    const setCardsClicked = useCallback(
+        (targetCards, cardToUpdate) => {
+            const updatedCards = targetCards.map((card) => {
+                if (card.id === cardToUpdate.id) {
+                    return { ...cardToUpdate, hasClicked: true }
+                }
+                return card
+            })
+            setCards(updatedCards)
+        },
+        [setCards]
+    )
 
-            return card
-        })
-
-        setCards(updatedCards)
+    const resetCards = () => {
+        setCards(generateNewCards(cardsData))
     }
 
-    const [cards, setCards] = useState(generateNewCards(cardsData))
-    const cardsInUse = cards.filter((card) => card.isCardInUse)
+    const incrementCurrentScore = () => setCurrentScore(currentScore + 1)
+
+    const resetGame = () => {
+        resetCards()
+        setCurrentScore(0)
+    }
+
+    
 
     return (
         <div>
+            <div className="current-score">Current score: {currentScore}</div>
             <Cards 
             cards={cards}
             cardsInUse={cardsInUse}
             setCardsClicked={setCardsClicked}
+            resetGame={resetGame}
+            incrementCurrentScore={incrementCurrentScore}
             />
         </div>
     )
