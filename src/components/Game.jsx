cf44151fd9fb6e99eee3b7fcb47713915cd6a18e
Quartezz <kamil.machel03@gmail.com>
2023-10-02 15:55:14 +0200

    code to shuffling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,21 @@
 import Cards from "./Cards"
+import { useState } from "react"
+import data from "../data/characters.json"
 
 export default function Game() {
+    function getShuffledCards(targetCards) {
+        const copy = [...targetCards]
+        for (let i = copy.length - 1; i > 0; i -= 1) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [copy[i], copy[j]] = [copy[j], copy[i]];
+        }
+
+        return copy;
+    }
+
+    const { characters } = data;
+    const [cards, setCards] = useState(getShuffledCards(characters));
+    
     return (
         <div>
             <Cards />
