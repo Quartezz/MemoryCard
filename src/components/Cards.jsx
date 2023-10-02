21d9ee3d8dc0fba3bba5780e96de626a7a5d8fb6
Quartezz <kamil.machel03@gmail.com>
2023-10-02 23:09:41 +0200

    functionality

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { forwardRef } from "react";
 import { getShuffledCards } from "./Generate";
+import "../styles/Cards.scss";
 
 const Cards = forwardRef(
   (
