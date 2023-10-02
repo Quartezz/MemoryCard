255135203d6dfb5b0d484b40ec26af8704ca6872
Quartezz <kamil.machel03@gmail.com>
2023-10-02 22:49:35 +0200

    created site structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Game from "./components/Game";
-import "./styles/App.scss"
+import "./styles/App.scss";
 
-export default function App () {
-    return (
-        <>
-        <Header />
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
         <Game />
-        </>
-    )
+      </main>
+    </div>
+  );
 }
