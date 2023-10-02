08f591693dc576146948a12fffb67f6592bcd2ad
Quartezz <kamil.machel03@gmail.com>
2023-10-02 13:51:56 +0200

    created main module root file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./styles/index.css"
+import "./styles/index.scss"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
