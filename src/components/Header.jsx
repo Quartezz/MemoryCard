2b6439fc583194535fbe5e91d13338fe471d2bc5
Quartezz <kamil.machel03@gmail.com>
2023-10-02 14:06:19 +0200

    created basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "../styles/Header.scss"
+
+export default function Header() {
+    return(
+        <header>
+            <img src="logo.png" alt="lol" />
+            <h1 className="title">Memory Card Game</h1>
+        </header>
+    )
+}
