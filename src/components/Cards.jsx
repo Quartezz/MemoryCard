4cb788f6c6c7ebdea93ad55bed0c8e4db24ec12b
Quartezz <kamil.machel03@gmail.com>
2023-10-02 20:03:25 +0200

    added prop types and li key

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,35 @@
-import data from "../data/cards.js";
 import "../styles/Cards.scss"
+import PropTypes from "prop-types"
 
-export default function Cards() {
-    const { characters } = data;
-    const [{ name, image }] = characters;
+Cards.PropTypes = {
+    cards: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string,
+            image: PropTypes.string,
+            hasClicked: PropTypes.bool,
+            isCardInUse: PropTypes.bool,
+            id: PropTypes.string,
+        })
+    ).isRequired,
+}
 
+export default function Cards({ cards }) {
+   
     return (
         <ul>
-            <li className="card">
-                <img src={`/characters/${image}`} alt="Character" />
-                <div className="character-name">{name}</div>
-            </li>
+            {cards.map(({ image, name, id }) => (
+                <li key={id} className="card">
+                    <button type="button" className="card-button">
+                        <img 
+                        className="character-image"
+                        src={`/characters/${image}`}
+                        alt="Champion"
+                         />
+                         <div className="character-name">{name}</div>
+                    </button>
+                </li>
+            ))}
         </ul>
     )
 }
+
