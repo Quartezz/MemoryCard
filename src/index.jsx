ffc4c651ffa0e727d2ea61c3a0df8b3b4f90bc41
Quartezz <kamil.machel03@gmail.com>
2023-10-02 14:05:53 +0200

    added Header

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import Header from "./components/Header"
 import "./styles/index.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Header />
     </React.StrictMode>
 )
 
